292e94721562c45c7c755a967b7ba632942b993a
Kevin Tou <toukevin@gmail.com>
2020-09-03 09:18:11 -0700

    Added movie button and styles

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { nominateMovie } from '../actions';
 
 const Movie = ({ movie, nominateMovie }) => {
+  const [displayTitle, setDisplayTitle] = useState(false);
+
   const handleClick = (e) => {
-    nominateMovie();
+    nominateMovie(movie);
+  };
+
+  const showTitle = (e) => {
+    setDisplayTitle(true);
+  };
+
+  const removeTitle = (e) => {
+    setDisplayTitle(false);
   };
 
   return (
     <div>
-      <div style={{ height: '230px' }}>
-        <img
-          src={movie.image ? URL.createObjectURL(movie.image) : movie.Poster}
-          style={{ height: '100%', maxWidth: '150px' }}
-          alt={movie.Title}
-        />
+      <div
+        className="movie-detail-wrapper"
+        onMouseEnter={showTitle}
+        onMouseLeave={removeTitle}
+      >
+        {displayTitle ? (
+          <div>
+            <h3>{movie.Title}</h3>
+            <p>({movie.Year})</p>
+          </div>
+        ) : (
+          <img
+            src={movie.image ? URL.createObjectURL(movie.image) : movie.Poster}
+            alt={movie.Title}
+            className="movie-detail-image"
+          />
+        )}
       </div>
-      <button onClick={handleClick}>Nominate</button>
+      <button className="movie-nomination-button" onClick={handleClick}>
+        Nominate
+      </button>
     </div>
   );
 };
