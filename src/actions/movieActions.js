da81e3da3a4ece47a390e66148ad1bec7d845938
Kevin Tou <toukevin@gmail.com>
2020-09-02 23:38:14 -0700

    Added search, nominate, and remove actions

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,1 +1,56 @@
 import axios from 'axios';
+
+export const NOMINATE_MOVIE = 'NOMINATE_MOVIE';
+
+export const nominateMovie = (movie) => (dispatch) => {
+  dispatch({ type: NOMINATE_MOVIE, payload: movie });
+};
+
+export const REMOVE_NOMINATION = 'REMOVE_NOMINATION';
+
+export const removeNomination = (movie) => (dispatch) => {
+  dispatch({ type: REMOVE_NOMINATION, payload: movie });
+};
+
+export const SEARCH_MOVIES = 'SEARCH_MOVIES';
+export const SEARCH_MOVIES_SUCCESS = 'SEARCH_MOVIES_SUCCESS';
+export const SEARCH_MOVIES_ERROR = 'SEARCH_MOVIES_ERROR';
+
+export const searchMovies = (query) => async (dispatch) => {
+  dispatch({ type: SEARCH_MOVIES });
+
+  try {
+    let { data } = await axios.get(
+      `http://www.omdbapi.com/?s=${query}&apikey=${process.env.REACT_APP_KEY}`
+    );
+
+    let movies = await Promise.all(
+      data.Search.map(async (movie) => {
+        try {
+          let img = await axios.get(
+            `http://img.omdbapi.com/?i=${movie.imdbID}&apikey=${process.env.REACT_APP_KEY}`,
+            {
+              responseType: 'blob',
+            }
+          );
+
+          return {
+            ...movie,
+            image: img.data,
+          };
+        } catch (err) {
+          return {
+            ...movie,
+            image: null,
+          };
+        }
+      })
+    );
+
+    console.log('movies: ', movies);
+
+    dispatch({ type: SEARCH_MOVIES_SUCCESS, payload: movies });
+  } catch (err) {
+    dispatch({ type: SEARCH_MOVIES_ERROR, payload: err });
+  }
+};
--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,7 +1,13 @@
-import {} from '../actions';
+import {
+  NOMINATE_MOVIE,
+  REMOVE_NOMINATION,
+  SEARCH_MOVIES,
+  SEARCH_MOVIES_SUCCESS,
+  SEARCH_MOVIES_ERROR,
+} from '../actions';
 
 const initialState = {
-  movies: [1, 2, 3],
+  movies: [],
   movie: {},
   nominations: [],
   isLoading: false,
@@ -10,6 +16,35 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case SEARCH_MOVIES:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case SEARCH_MOVIES_SUCCESS:
+      return {
+        ...state,
+        movies: [...action.payload],
+        isLoading: false,
+      };
+    case SEARCH_MOVIES_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    case NOMINATE_MOVIE:
+      return {
+        ...state,
+        nominations: [...state.nominations, action.payload],
+      };
+    case REMOVE_NOMINATION:
+      return {
+        ...state,
+        nominations: state.nominations.filter(
+          (movie) => movie === action.payload
+        ),
+      };
     default:
       return state;
   }
